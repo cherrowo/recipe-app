1ae9a8cbeaf7c3bf6451011f09382d60c90bab89
churrowo <li.cheryl.2706@gmail.com>
2020-09-29 22:03:10 -0600

    Created Recipe Components
    Takes data from 'hits' contained in API in order to get recipe data. So far we have Image, Title, Calories. Need to work on search next.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
+import Recipe from './recipe';
+
 const App = () =>{
 
   const APP_ID = '1feeff86';
   const APP_KEY = 'bf8f94d35627479acf333a305b5edfec';
 
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]); //stores recipes in array
 
-  const [counter, setCounter] = useState(0);
-  useEffect(() => {
-    console.log('Effect has been run');
+  useEffect( () => {
+    getRecipes();
   }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json(); //await because of delay from external API, json formats it into easy to work with format
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
   return(
     <div className="App">
       <form className="search-form">
         <input type="text" className="search-bar"/>
         <button type="submit" className="search-button">Search</button>
       </form>
-        <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      {recipes.map(recipe =>(
+        <Recipe
+          key={recipe.recipe.label} //unique ID for each item so React renders faster
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}/>
+      ))};
     </div>
   );
 }
