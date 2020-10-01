421537a5989d3af6456c98a4e46ffef7697813f1
churrowo <li.cheryl.2706@gmail.com>
2020-09-30 22:47:46 -0600

    Functionality
    Added search, styling

--- src/App.js
+++ src/App.js
@@ -9,31 +9,49 @@ const App = () =>{
   const APP_KEY = 'bf8f94d35627479acf333a305b5edfec';
 
   const [recipes, setRecipes] = useState([]); //stores recipes in array
+  const [search, setSearch] = useState(''); //allows users to use search bar
+  const [query, setQuery] = useState('chicken'); //creates queries
 
   useEffect( () => {
     getRecipes();
-  }, []);
+  }, [query]); //updates afetr form submitted
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json(); //await because of delay from external API, json formats it into easy to work with format
     setRecipes(data.hits);
     console.log(data.hits);
   };
 
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search);
+    setSearch(''); //resets the search bar
+  }
   return(
     <div className="App">
-      <form className="search-form">
-        <input type="text" className="search-bar"/>
+      <form className="search-form" onSubmit={getSearch}>
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
         <button type="submit" className="search-button">Search</button>
       </form>
-      {recipes.map(recipe =>(
-        <Recipe
-          key={recipe.recipe.label} //unique ID for each item so React renders faster
-          title={recipe.recipe.label} 
-          calories={recipe.recipe.calories} 
-          image={recipe.recipe.image}/>
-      ))};
+      <div className="recipes">
+        {recipes.map(recipe =>(
+          <Recipe
+              key={recipe.recipe.label} //unique ID for each item so React renders faster
+              title={recipe.recipe.label} 
+              calories={recipe.recipe.calories} 
+              image={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+            />
+        ))};
+
+      </div>
+
     </div>
   );
 }
--- src/recipe.js
+++ src/recipe.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import style from './recipe.module.css';
 
-
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return(
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
             <p>Calories: {calories}</p>
-            <img src={image} alt=""/>
+            <img src={image} alt=""  className={style.image}/>
+            <ul>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ul>
         </div>
     );
 }
